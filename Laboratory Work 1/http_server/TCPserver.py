2a074b23b3c099405c80e557c4ac86345797f2cb
Patricia Moraru <patriciamoraruv@gmail.com>
2025-10-11 14:51:29 +0300

    add: 501 response

--- Laboratory Work 1/http_server/TCPserver.py
+++ Laboratory Work 1/http_server/TCPserver.py
@@ -54,11 +54,25 @@ class HTTPServer(TCPServer):
         """
         request = HTTPRequest(data)
 
-        handler = getattr(self, 'handle_%s' % request.method)
+        try:
+            handler = getattr(self, 'handle_%s' % request.method)
+        except AttributeError:
+            handler = self.HTTP_501_handler
 
         response = handler(request)
 
         return response
+    
+    def HTTP_501_handler(self, request):
+        response_line = self.response_line(status_code=501)
+
+        response_headers = self.response_headers()
+
+        blank_line = b"\r\n"
+
+        response_body = b"<h1>501 Not Implemented</h1>"
+
+        return b"".join([response_line, response_headers, blank_line, response_body])
     
     def handle_GET(self, request):
         pass
